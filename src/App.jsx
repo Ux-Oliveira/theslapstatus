3c049177970811911f7b50812d2cf324d35bbce8
Henrique O. <lemonheadch@outlook.com>
2026-05-19 18:23:27 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import SlapStatus from "./SlapStatus"
-import Test from "./Test"
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SlapStatus />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   )
