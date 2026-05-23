76b808805d0c24f2a7676cffeee594bb4a7ed2ac
Henrique O. <lemonheadch@outlook.com>
2026-05-23 12:20:35 -0300

    Update generate-video.js

--- api/generate-video.js
+++ api/generate-video.js
@@ -82,7 +82,7 @@ mood = sanitizeText(mood).slice(0, 29)
         makeTextImage(status, statusPath, 55, "black", 135, 880, "ArialCustom")
         makeTextImage(status2, status2Path, 55, "black", 135, 950, "ArialCustom")
         makeTextImage(status3, status3Path, 55, "black", 135, 1020, "ArialCustom")
-        makeTextImage(mood, moodPath, 55, "black", 135, 1320, `"ArialCustom","EmojiCustom",sans-serif`)
+        makeTextImage(`Mood: ${mood}`, moodPath, 55, "purple", 135, 1320, `"ArialCustom","EmojiCustom",sans-serif`, "bold")
     } catch (err) {
 
         console.error("Image error:", err)
